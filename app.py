cb6a108c819ef0129b2d602c3ee9b994adde19d6
Igor Bezerra <igor.bezerra96@gmail.com>
2017-09-23 03:05:48 +0300

    Creating class, file reader and started to create front-end

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python
-import re
+import abcdbank
 
-def check_valid_card_number(candidate):
-    pattern = r'((?!.*(\d)\2{3,})[4,5,6](\d{15}))$'
-    card_number = candidate.replace("-", "")
-    if re.match(pattern, str(card_number)):
-        return True
-    return False
+def read_cards_typed():
+    n = input("Enter quantity of credit cards numbers that you will type:")
 
-def read_card_number():
-    card_number = input("Enter your credit card number: ")
+    for i in range(0, int(n)):
+        card_number = input("Enter your credit card number: ")
+        card = abcdbank.CreditCard(card_number)
+        print card.check_valid_card_number()
 
-    if check_valid_card_number(card_number):
-        print "It's valid!"
+def read_cards_file(path):
+    cards_file = open(path, "r")
+    n = int(cards_file.read(1).strip())
+    cards_file_len = sum(1 for line in cards_file)
+
+    if 1 <= n <= 100 and n == cards_file_len-1:
+        cards_file.seek(2)
+        for line in cards_file:
+            card = abcdbank.CreditCard(line.strip())
+            print line.rstrip()+'-'+card.check_valid_card_number()
+    cards_file.close()
+
+if __name__ == "__main__":
+    user_input = input("Choose your option:\nR - Read a file\nT - Type credit cards numbers\n")
+
+    if str(user_input) == 'R':
+        file_path = input("Enter the path of your file: ")
+        read_cards_file(file_path)
+    elif user_input == 'T':
+        read_cards_typed()
     else:
-        print "Sorry, the given credit card number is not valid."
-
-read_card_number()
+        print "Invalid Option"
