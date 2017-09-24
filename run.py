1045e72b3d76513ca53435fb493d827c45e58c3b
Igor Bezerra <igor.bezerra96@gmail.com>
2017-09-24 18:39:53 +0300

    updating configuration

--- run.py
+++ run.py
@@ -1,21 +1,4 @@
-#!/usr/bin/env python
-import os
-
-from waitress import serve
-from pyramid.settings import asbool
-import card_validator
-
-
-# server info
-host = '0.0.0.0'
-port = int(os.environ.get('PORT', '8080'))
-
-settings = {}
-settings['pyramid.includes'] = ['pyramid_tm']
-# debug mode
-if asbool(os.environ.get('DEBUG', 0)):
-    settings['pyramid.reload_templates'] = True
-    settings['pyramid.debug_templates'] = True
-    settings['pyramid.includes'].append('pyramid_debugtoolbar')
-
-serve(card_validator.main({}, **settings), host=host, port=port)
+#!/bin/bash
+set -e
+python setup.py develop
+python runapp.py
