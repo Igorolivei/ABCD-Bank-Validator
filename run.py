d70108cb16ce0aa98a058926167b4b6911b204bb
Igor Bezerra <igor.bezerra96@gmail.com>
2017-09-24 17:22:42 +0300

    creating procfile and run.py

---
+++ run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+
+from waitress import serve
+from pyramid.settings import asbool
+import card_validator
+
+
+# server info
+host = '0.0.0.0'
+port = int(os.environ.get('PORT', '8080'))
+
+settings = {}
+settings['pyramid.includes'] = ['pyramid_tm']
+# debug mode
+if asbool(os.environ.get('DEBUG', 0)):
+    settings['pyramid.reload_templates'] = False
+    settings['pyramid.debug_templates'] = False
+    settings['pyramid.includes'].append('pyramid_debugtoolbar')
+
+serve(card_validator.main({}, **settings), host=host, port=port)
